beda71c70ca6f64c70bcab1b24d23ce800764446
kim <tksok2009@gmail.com>
2019-05-01 21:08:36 +0900

    add  --mode development

--- src/admin.tsx
+++ src/admin.tsx
@@ -3,6 +3,11 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./component/Hello";
 
+let num :number;
+
+num = 1;
+console.log(num);
+
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
     document.getElementById("example")
