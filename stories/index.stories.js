1ec0bf70300f3e276357b1baac983d6005b95e79
kim <tksok2009@gmail.com>
2019-05-28 17:44:15 +0900

    r

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,10 +6,14 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
+import {Hello} from "../src/component/Hello";
+
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => <Hello compiler="TypeScript" framework="React" />)
+
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
